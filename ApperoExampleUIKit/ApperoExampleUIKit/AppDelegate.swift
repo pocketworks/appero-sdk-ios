badf7256baaaf68f96080a9cb858aa10f5c9bb1c
Rory Prior <rory@thinkmac.co.uk>
2024-05-29 13:39:15 +0100

    feat: UIKit demo app working

--- ApperoExampleUIKit/ApperoExampleUIKit/AppDelegate.swift
+++ ApperoExampleUIKit/ApperoExampleUIKit/AppDelegate.swift
@@ -20,7 +20,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             apiKey: "79c589ac-325d-4621-95aa-0a4c7ecdc5ba",
             clientId: "2ffab699-33a7-4bef-9473-3f661cf22553"
         )
-        Appero.instance.theme = DarkTheme()
+        
+        Appero.instance.theme = DefaultTheme()  // set the theme to use for the Appero UI
+        Appero.instance.ratingThreshold = 10   // the threshold at which point we'll trigger the Appero UI
+        
+        // for the demo app we want to reset things on each launch, you likely wouldn't want to do this in a real app.
+        Appero.instance.resetExperienceAndPrompt()
         
         return true
     }
--- ApperoExampleUIKit/ApperoExampleUIKit/ViewController.swift
+++ ApperoExampleUIKit/ApperoExampleUIKit/ViewController.swift
@@ -6,17 +6,19 @@
 //
 
 import UIKit
+import SwiftUI
 import Appero
 
 class ViewController: UIViewController {
 
     @IBOutlet var themePicker: UISegmentedControl!
+    var hostingController: UIHostingController<ApperoPresentationView>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        themePicker.selectedSegmentIndex = 2
+        themePicker.selectedSegmentIndex = 0
     }
 
     
@@ -31,8 +33,50 @@ class ViewController: UIViewController {
         }
     }
 
-    @IBAction func openAppero(sender: Any?) {
+    @IBAction func positiveVibes(sender: Any?) {
+        Appero.instance.log(experience: .strongPositive)
+        if Appero.instance.shouldShowAppero {
+            openAppero()
+        }
+    }
+    
+    func openAppero() {
+        let apperoRatingView = ApperoPresentationView {
+            // remove the child view controller when the panel is dismissed
+            self.hostingController?.willMove(toParent: nil)
+            self.hostingController?.view.removeFromSuperview()
+            self.hostingController?.removeFromParent()
+        }
         
+        hostingController = UIHostingController(rootView: apperoRatingView)
+        hostingController?.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let hostingController = hostingController {
+            hostingController.view.backgroundColor = .clear
+            addChild(hostingController)
+            view.addSubview(hostingController.view)
+            hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+            hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+            hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+            hostingController.didMove(toParent: self)
+        }
+    }
+}
+
+struct ApperoPresentationView: View {
+    
+    let onDismiss: (()->())?
+     
+    @State var presented = true
+    
+    var body: some View {
+        EmptyView()
+        .sheet(isPresented: $presented, onDismiss: {
+            onDismiss?()
+        }) {
+            ApperoRatingView(productName: "ApperoExampleUIKit")
+        }
     }
 }
 
